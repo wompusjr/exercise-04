39caefc5ced1a225aaf1cc2bb6f62033780ef0db
wompusjr <june.burke@utexas.edu>
2025-02-17 01:28:35 -0600

    local commit 2:17 1:28

--- exercise04.R
+++ exercise04.R
@@ -1,7 +1,36 @@
-load.directory <- function(x){
-  tmp <- read.delim(x)
+library(tidyverse)
+#step one
+load.dictionary <- function(x){
+  x <- read.delim(x)
+    as.vector(x$words)
 }
-valid_test <- load.directory("collins-scrabble-words-2019.txt")  
-str(valid_test)
-solution_test <- load.directory("google-10000-english-usa-no-swears.txt")
+valid_list <- load.dictionary("collins-scrabble-words-2019.txt")  
+str(valid_list)
+solution_test <- load.dictionary("google-10000-english-usa-no-swears.txt")
 str(solution_test)
+#step two
+solution_list <- intersect(valid_list, solution_test)
+str(solution_list)
+#step three
+pick.solution <- function(x){
+  x <- x[nchar(x) == 5]
+  x <- sample(x, 1)
+  x <- strsplit(x, "")
+}
+solution <- pick.solution(solution_list)
+str(solution)
+#step four
+evaluate_guess <- function(x, solution) {
+  
+}
+play_wordle <- function(x = solution,y = valid_list, num_guesses=6){
+    intro <- paste("You have", num_guesses, "chances to guess a 5 letter word")
+    print(intro)
+    cat("Letters left:", paste(LETTERS, collapse = " "))
+    guess <- readline("Write your guess, then press <enter> ")
+    guess <- toupper(guess)
+    cat(ifelse(nchar(guess) != 5 | guess[guess %in% valid_list],
+               "This question doesn't match the criteria. It must be a valid word and exactly 5 characters long",
+               " "))
+}
+play_wordle()
